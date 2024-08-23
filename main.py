0d4ffa2176ef45794950361928b1a8365fffc6e4
Hieu <hieuhovan954@gmail.com>
2024-08-23 16:20:09 +0700

    add allowed hosts middleware

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from routers import lazada
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
+load_dotenv()
 
+allowed_hosts = os.getenv('ALLOWED_HOSTS')
 app = FastAPI()
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=allowed_hosts)
 app.include_router(lazada.router, prefix="/lazada")
 
 
