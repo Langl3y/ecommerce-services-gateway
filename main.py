7ce02aa65369c381afa47841fb202691b4909b60
Hieu <hieuhovan954@gmail.com>
2024-08-26 14:05:22 +0700

    update endpoints for tiki APIs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from routers import lazada
+from routers import lazada, tiki
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 load_dotenv()
@@ -11,15 +11,35 @@ allowed_hosts = os.getenv('ALLOWED_HOSTS')
 app = FastAPI()
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=allowed_hosts)
 app.include_router(lazada.router, prefix="/lazada")
+app.include_router(tiki.router, prefix="/tiki")
 
 
 @app.get("/")
-def root():
-    return {"API": [
+def api_list():
+    return {"all_endpoints": [
         "/docs",
         "/lazada/get_token",
         "/lazada/refresh_token",
         "/lazada/order/get",
         "/lazada/orders/get",
-        "/lazada/finance/transaction/detail/get"
+        "/lazada/finance/transaction/detail/get",
+        "/tiki/get_token"
+    ]}
+
+
+@app.get("/lazada")
+def lazada_api_list():
+    return {"API": [
+        "/get_token",
+        "/refresh_token",
+        "/order/get",
+        "/orders/get",
+        "/transaction/detail/get"
+    ]}
+
+
+@app.get("/tiki")
+def tiki_api_list():
+    return {"API": [
+        "/get_token"
     ]}
